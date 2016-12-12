683f94bcfe59907082041a968dd173df54b487ad
Cynthia L. McLaughlin <spirielle@hotmail.com>
2016-12-11 19:31:56 -0500

    The "Go back in time" now reloads the scene

---
+++ TimeLines/Assets/Scripts/Buttons.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
